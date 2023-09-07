f74d9463348e6286dbf49826f9be738ad224f3b0
Antônio Ryan Araújo Ramos <antonioryanaraujo@gmail.com>
2023-09-07 13:43:54 -0300

    Primeiro deploy da aplicação

--- medicSearchAdmin/settings/production.py
+++ medicSearchAdmin/settings/production.py
@@ -5,11 +5,10 @@ DEBUG = True
 SECRET_KEY = 's+3(^Ca*0@+aww=v4tlr*!hqr@*q0gibs*o-z1-+&615ob)k^F'
 
 ALLOWED_HOSTS = ['https://med-consult-63b45cb0f41f.herokuapp.com'] 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
