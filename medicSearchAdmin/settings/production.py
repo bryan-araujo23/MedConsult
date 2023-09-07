df74d47498ea7930ecf288d1471033ed6303b439
Antônio Ryan Araújo Ramos <antonioryanaraujo@gmail.com>
2023-09-07 11:04:43 -0300

    config. iniciais do deploy heroku

--- medicSearchAdmin/settings/production.py
+++ medicSearchAdmin/settings/production.py
@@ -1,17 +1,16 @@
 from .settings import *
-DEBUG = False
+
+DEBUG = True
 
 SECRET_KEY = 's+3(^Ca*0@+aww=v4tlr*!hqr@*q0gibs*o-z1-+&615ob)k^F'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'medicSearchAdmin',
-        'USER': 'postgres',
-        'PASSWORD': 123, 
-        'HOST': 'localhost',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
